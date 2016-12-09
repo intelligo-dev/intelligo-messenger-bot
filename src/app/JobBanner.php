689277969fa3b9cf6473e463f2f355a8cc235722
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-12-10 02:02:14 +0800

    ажилын зураг модел

--- src/app/JobBanner.php
+++ src/app/JobBanner.php
@@ -2,9 +2,143 @@
 
 namespace App;
 
+use Image;
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class JobBanner extends Model
 {
     //
+    protected $table = "job_banner";
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'path', 'thumbnail_path'];
+
+    /**
+     * @var
+     */
+    protected $file;
+
+    /**
+     * @var
+     */
+    protected $name;
+
+
+    public function job() {
+      return $this->belongsTo('App\Job');
+    }
+
+
+    /**
+     *
+     * @param UploadedFile $file
+     * @return static
+     */
+    public static function fromFile(UploadedFile $file) {
+        $photo = new static;
+
+        $photo->file = $file;
+
+        $photo->fill([
+            'name' => $photo->setFileName(),
+            'path' => $photo->filePath(),
+            'thumbnail_path' => $photo->thumbnailPath()
+        ]);
+
+        // return the photo
+        return $photo;
+
+    }
+
+
+    /**
+     * Get the banner photos base directory.
+     */
+    public function baseDir() {
+        return 'src/public/JobBanner/photos';
+    }
+
+
+    /**
+     * Get the name and extension of the banner photo.
+     *
+     * @return string
+     */
+    public function setFileName() {
+        // Get the file name original name
+        // and encrypt it with sha1
+        $hash = sha1 (
+            $this->file->getClientOriginalName()
+        );
+
+        // Get the extension of the photo.
+        $extension = $this->file->getClientOriginalExtension();
+
+        // Then set name = merge those together.
+        return $this->name = "{$hash}.{$extension}";
+    }
+
+
+
+    /**
+     *  Return the full file path of the banner photo, with the name.
+     *
+     * @return string
+     */
+    public function filePath() {
+        return $this->baseDir() . '/' . $this->name;
+        // Ex: 'BannerPhoto/photos/foo.jpg'
+    }
+
+
+    /**
+     * Return the full file thumbnail path of the banner photo, with the name.
+     *
+     * @return string
+     */
+    public function thumbnailPath() {
+        return $this->baseDir() . '/tn-' . $this->name;
+        // Ex: 'BannerPhoto/photos/tn-foo.jpg'
+    }
+
+
+    /**
+     *
+     * @return $this
+     */
+    public function upload() {
+        $this->file->move($this->baseDir(), $this->name);
+
+        $this->makeThumbnail();
+
+        return $this;
+    }
+
+    protected function makeThumbnail() {
+        Image::make($this->filePath())
+            ->fit(2000, 800)
+            //->resize(null, 400, function ($constraint) {
+             //   $constraint->aspectRatio();
+             //   $constraint->upsize();
+            //})
+            ->save($this->thumbnailPath());
+    }
+
+    public function delete() {
+
+        $image = $this->path;
+        $thumbnail_image = $this->thumbnail_path;
+
+        \File::delete([
+            $image,
+            $thumbnail_image
+        ]);
+
+        parent::delete();
+    }
+
 }
--- src/app/JobPhoto.php
+++ src/app/JobPhoto.php
@@ -3,8 +3,55 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 
 class JobPhoto extends Model
 {
     //
+    protected $table = "job_photos";
+
+    /**
+     * @var array
+     */
+    protected $fillable = ['name', 'path', 'thumbnail_path'];
+
+
+    /**
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function job() {
+        return $this->belongsTo('App\Job');
+    }
+
+    public function baseDir() {
+        return 'src/public/JobPhotos/photos';
+    }
+
+
+    public function setNameAttribute($name) {
+        $this->attributes['name'] = $name;
+
+        //
+        $this->path = $this->baseDir() . '/' . $name;
+
+        //
+        $this->thumbnail_path = $this->baseDir() . '/th-' . $name;
+    }
+
+
+    /**
+     * @throws \Exception
+     */
+    public function delete() {
+
+        \File::delete([
+            $this->path,
+            $this->thumbnail_path
+        ]);
+
+        parent::delete();
+    }
+
 }
