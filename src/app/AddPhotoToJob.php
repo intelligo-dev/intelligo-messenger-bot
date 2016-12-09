95f8aa92aaa211d5d7d09914c1d271478335ec92
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-12-10 01:51:34 +0800

    ажлыг зураг нэмэх класс

--- src/app/AddPhotoToJob.php
+++ src/app/AddPhotoToJob.php
@@ -2,9 +2,50 @@
 
 namespace App;
 
-use Illuminate\Database\Eloquent\Model;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
 
-class AddPhotoToJob extends Model
+class AddPhotoToJob
 {
     //
+
+    /**
+     * @var Ажил
+     */
+    protected $job;
+
+    protected $file;
+
+    public function __construct(Job $job, UploadedFile $file, Thumbnail $thumbnail = null) {
+        $this->job = $job;
+        $this->file = $file;
+        $this->thumbnail = $thumbnail ?: new Thumbnail;
+    }
+
+    public function save() {
+
+        $photo = $this->job->addPhoto($this->makePhoto());
+
+        $this->file->move($photo->baseDir(), $photo->name);
+
+        $this->thumbnail->make($photo->path, $photo->thumbnail_path);
+    }
+
+
+
+    protected function makePhoto() {
+       return new JobPhoto(['name' => $this->makeFilename()]);
+    }
+
+
+    protected function makeFilename() {
+
+        $name = sha1 (
+            time() . $this->file->getClientOriginalName()
+        );
+
+        $extension = $this->file->getClientOriginalExtension();
+
+        return "{$name}.{$extension}";
+    }
+
 }
