9d4f202708f310e92d73cdd90af21c9cbba11846
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2016-12-11 12:31:52 +0800

    нүүр хуудасд онцлох, шинэ ажилууд харуулсан

--- web/resources/views/jobs/home.blade.php
+++ web/resources/views/jobs/home.blade.php
@@ -18,7 +18,113 @@
         </div>
 
         <div class="col-md-12" id="Flyers-ShowAll-Container">
+            <h3>Санал болгох</h3>
+            @foreach($job as $jobs)
+                <div class="col-sm-6 col-md-3">
+                    <div class="row">
+                        <div class="ui link cards" id="Travel-Flyer-Display-Cards">
+                            <div class="card" id="Flyer-Card">
+                                <div class="image">
+                                    <a href="{{ route('jobs.show', $jobs->title) }}">
+                                        @foreach ($jobs->bannerPhotos as $photo)
+                                            <img src="/{{ $photo->thumbnail_path }}" alt="{{ $jobs->owner->username }}" data-id="{{ $photo->id }}">
+                                        @endforeach
+                                    </a>
+                                </div>
+                                <div class="content">
+                                    <a href="{{ route('jobs.show', $jobs->title) }}">
+                                        <h4 class="ui header"> {{ str_limit($jobs->title, $limit = 80, $end = '...') }}</h4>
+                                    </a>
+                                    <div class="meta"><br>
+                                        @foreach ($jobs->owner->Profilephotos as $photo)
+                                            <a href="{{ route('users.show', $jobs->owner->id) }}" class="avatar">
+                                                <img class="ui avatar image mini" src="/{{ $photo->thumbnail_path }}" alt="{{ $jobs->owner->username }}'s Profile Picture">
+                                            </a>
+                                        @endforeach
+                                        <a href="{{ route('users.show', $jobs->owner->id) }}">
+                                            <span id="Flyer-Username-Index-Page">{{ $jobs->owner->username }}</span>
+                                        </a>
+                                    </div><br>
+                                    <div class="meta">
+                                        <a>{{ str_limit($jobs->excerpt, $limit = 79, $end = '...') }}</a>
+                                    </div>
+                                </div>
+                                <div class="extra content">
+                                    <span class="right floated">
+                                        {{ prettyDate($jobs->created_at) }}
+                                    </span>
+                                    <span>
+                                        <i class="thumbs up icon"></i>{{ $jobs->likes->count() }}&nbsp;
+                                        @if ($user && $user->owns($jobs))
+                                            <a href="{{ route('jobs.edit', $jobs->id) }}">
+                                                <i class="edit icon"></i>
+                                            </a>
+                                        @endif
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
 
+
+        <div class="col-md-12" id="Flyers-ShowAll-Container">
+            <h3>Шинэ ажлууд</h3>
+            @foreach($job as $jobs)
+                <div class="col-sm-6 col-md-3">
+                    <div class="row">
+                        <div class="ui link cards" id="Travel-Flyer-Display-Cards">
+                            <div class="card" id="Flyer-Card">
+                                <div class="image">
+                                    <a href="{{ route('jobs.show', $jobs->title) }}">
+                                        @foreach ($jobs->bannerPhotos as $photo)
+                                            <img src="/{{ $photo->thumbnail_path }}" alt="{{ $jobs->owner->username }}" data-id="{{ $photo->id }}">
+                                        @endforeach
+                                    </a>
+                                </div>
+                                <div class="content">
+                                    <a href="{{ route('jobs.show', $jobs->title) }}">
+                                        <h4 class="ui header"> {{ str_limit($jobs->title, $limit = 80, $end = '...') }}</h4>
+                                    </a>
+                                    <div class="meta"><br>
+                                        @foreach ($jobs->owner->Profilephotos as $photo)
+                                            <a href="{{ route('users.show', $jobs->owner->id) }}" class="avatar">
+                                                <img class="ui avatar image mini" src="/{{ $photo->thumbnail_path }}" alt="{{ $jobs->owner->username }}'s Profile Picture">
+                                            </a>
+                                        @endforeach
+                                        <a href="{{ route('users.show', $jobs->owner->id) }}">
+                                            <span id="Flyer-Username-Index-Page">{{ $jobs->owner->username }}</span>
+                                        </a>
+                                    </div><br>
+                                    <div class="meta">
+                                        <a>{{ str_limit($jobs->excerpt, $limit = 79, $end = '...') }}</a>
+                                    </div>
+                                </div>
+                                <div class="extra content">
+                                    <span class="right floated">
+                                        {{ prettyDate($jobs->created_at) }}
+                                    </span>
+                                    <span>
+                                        <i class="thumbs up icon"></i>{{ $jobs->likes->count() }}&nbsp;
+                                        @if ($user && $user->owns($jobs))
+                                            <a href="{{ route('jobs.edit', $jobs->id) }}">
+                                                <i class="edit icon"></i>
+                                            </a>
+                                        @endif
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+
+
+        <div class="col-md-12" id="Flyers-ShowAll-Container">
+            <h3>Санал болгох</h3>
             @foreach($job as $jobs)
                 <div class="col-sm-6 col-md-3">
                     <div class="row">
